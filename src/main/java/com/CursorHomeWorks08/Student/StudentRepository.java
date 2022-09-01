84fac9d52693ec4a1f5a7a7ac05def1f770bea0f
ANDREY <andrey.kiyas@gmail.com>
2022-09-01 23:20:59 +0300

    PR01 "Homework 08"

--- src/main/java/com/CursorHomeWorks08/Student/StudentRepository.java
+++ src/main/java/com/CursorHomeWorks08/Student/StudentRepository.java
@@ -1,25 +1,6 @@
 package com.CursorHomeWorks08.Student;
 
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
-
-import java.util.List;
 
 public interface StudentRepository extends CrudRepository<Student, Long>{
-
-//        QueryByExampleExecutor<Student>, JpaSpecificationExecutor<Student>{
-
-//    List<Student> findByName(String name);
-//
-//    default Student saveWithNameSuffix(Student student) {
-//        student.setName(student.getName() + " TM");
-//        return save(student);
-//    }
-//
-//    @Modifying
-//    @Query("select p from Student p where p.age > ?1")
-//    List<Student> findStudentsWithHighPrices(Integer age);
 }
